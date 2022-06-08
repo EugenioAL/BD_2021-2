919aa4eacb7a3c6aeaa86323de1db8299dd674ae
Eugênio <eaof@icomp.ufam.edu.br>
2022-06-07 22:37:52 -0300

    Dados dos produtos coletados

--- tp1/parser.py
+++ tp1/parser.py
@@ -1,32 +1,27 @@
-
 class Product:
-    def __init__(self,id,asin):
-        self.id = id
-        self.asin = asin
+    def __init__(self):
+        self.id = 0
+        self.asin = 0
         self.title = ""
         self.group = ""
         self.salesrank = 0
-
-    def toDB(self):
-        return 
+        self.categoria = ''
 
 class Similar:
     def __init__(self):
         self.product_id = 0
         self.product_tog_id = 0
 
-class Subcat:
+class Grupo:
+    def __init__(self) -> None:
+        self.nome = ''
+
+
+class Categorias:
     def __init__(self):
         self.cat_id = 0
         self.cat_name = ""
         self.subcat_id = 0
-        self.subcat_name = ""
-
-class Cat_prod:
-    def __init__(self):
-        self.catid = 0
-        self.product_asin = ""
-
 class Reviews:
     def __init__(self):
         self.product_asin = ""
@@ -36,34 +31,76 @@ class Reviews:
         self.votes = 0
         self.helpf = 0
 
-def splitBySpace(dataText):
-    dataLines = dataText.split('  ', -1)
+def splitBySpace(dataText, productList,grupoList,reviwsList,categoriasList,similarList):
+    dataLines = dataText.split(' ', -1)
+    i = 0
+    #while (i < len(dataLines)):
+     #   if(dataLines[i] == ' '):
+      #      del dataLines[i]
+       # else:
+        #    i+=1
     print(dataLines)
     return dataLines
 
-def splitByLine(dataText):
+def splitByLine(dataText,productList,grupoList,reviwsList,categoriasList,similarList):
+    tmpProduct = Product()
+    tmpGrupos = Grupo()
+    tmpReviews = Reviews()
+    tmpCategorias = Categorias()
+    tmpSimilar = Similar()
+    Lista = []
+    
     dataLines = dataText.split('\n', -1)
     for i in range(len(dataLines)):
-        splitBySpace(dataLines[i])
+        print(  "linha:\t",i)
+        print(dataLines[i])
+    tmpProduct.id = int(dataLines[0])
+    print("Produto id:\t",tmpProduct.id)
+    Lista = dataLines[1].split("ASIN: ",-1)
+    tmpProduct.asin = int(Lista[1])
+    print("Produto asin:\t",tmpProduct.asin)
+    Lista =  dataLines[2].split("title: ",-1)
+    if(len(Lista) > 1):
+        tmpProduct.title = Lista[1]
+        print("Produto titulo:\t",tmpProduct.title)
+        Lista =  dataLines[3].split("group: ",-1)
+        tmpGrupos.nome = Lista[1]
+        print("Grupo:\t\t",tmpGrupos.nome)
+        Lista =  dataLines[4].split("salesrank: ",-1)
+        tmpProduct.salesrank = int(Lista[1])
+        print("Salesrank:\t",tmpProduct.salesrank)
+    else:
+        tmpProduct.title = Lista[0]
+        print("Produto titulo:\t",tmpProduct.title)
+
+
 
 
 
 def parser():
+    tmpProduct = []
+    customer = []
+    reviews = []
+    categorias = []
+    similares = []
+
     with open('ptest.txt') as file:
         file_contents = file.read()
-        file_split_by_id = file_contents.split('Id: ',-1)
+        file_split_by_id = file_contents.split('Id:   ',-1)
     
-    for i in range(len(file_split_by_id)):
-        splitByLine(file_split_by_id[i])
+    i=1
+    
+    while(i < len(file_split_by_id)):
+        splitByLine(file_split_by_id[i],tmpProduct, customer, reviews,categorias,similares)
+        i+=1
 
-
-    fr = "|Books[283155]|Subjects[1000]|Religion & Spirituality[22]|Christianity[12290]|Clergy[12360]|Preaching[12368]"
-    fd = fr.split('|',-1)
+    #fr = "|Books[283155]|Subjects[1000]|Religion & Spirituality[22]|Christianity[12290]|Clergy[12360]|Preaching[12368]"
+    #fd = fr.split('|',-1)
 
     
-    cd = fd[1].split('[',1)
+    #cd = fd[1].split('[',1)
     
-    catID = int(''.join(filter(str.isdigit,fd[1])))
+    #catID = int(''.join(filter(str.isdigit,fd[1])))
 
 
 
