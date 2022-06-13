e316ea185a36ca628b29b8348cb00ce1b0e60dd9
Eugênio <eaof@icomp.ufam.edu.br>
2022-06-13 00:30:42 -0300

    Parser completo

--- tp1/parser.py
+++ tp1/parser.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import re
 
 class Product:
     def __init__(self):
@@ -22,8 +22,9 @@ class Grupo:
 
 class Categorias:
     def __init__(self):
-        self.cat_id = 0
-        self.cat_name = ""
+        self.catpai = ""
+        self.catpai_id = 0
+        self.subcat_name = ""
         self.subcat_id = 0
 class Reviews:
     def __init__(self):
@@ -74,14 +75,51 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
             i+=1
         Lista = dataLines[6].split("categories: ",-1)
         catQt = int(Lista[1])
+        i = 1
+        while(i <= catQt):
+            Lista = re.findall(r'[a-z &\-A-Z]+', dataLines[6+i])
+            Lista.pop(0)
+            x = len(Lista)
+            Lista2 = re.findall(r'[0-9]+', dataLines[6+i])
+            a = 0
+            while(a < x):
+                if(a > 0):
+                    tmpCategorias.catpai = Lista[a-1]
+                    tmpCategorias.catpai_id = int(Lista2[a-1])
+                    tmpCategorias.subcat_name = Lista[a]
+                    tmpCategorias.subcat_id = int(Lista2[a])
+                else:
+                    tmpCategorias.catpai = Lista[a]
+                    tmpCategorias.catpai_id = int(Lista2[a])
+                    tmpCategorias.subcat_name = Lista[a]
+                    tmpCategorias.subcat_id = (Lista2[a])
+                categoriasList.append(Categorias())
+                categoriasList[len(categoriasList)-1].catpai = tmpCategorias.catpai
+                categoriasList[len(categoriasList)-1].catpai_id =tmpCategorias.catpai_id
+                categoriasList[len(categoriasList)-1].subcat_name = tmpCategorias.subcat_name
+                categoriasList[len(categoriasList)-1].subcat_id = tmpCategorias.subcat_id
+                a+=1
+            i+=1
         x = 6 + catQt+1
-        Lista = dataLines[x].split("reviews: total: ",-1)
-        Lista = Lista[1].split("  ",-1)
+        Lista = re.findall(r"[0-9]+",dataLines[x])
         reviewsQt = int(Lista[0])
-        finalLine = reviewsQt + x
+        finalCategorias = reviewsQt + x
         x += 1
-        print(Lista)
-        print(reviewsQt)
+        while(x <= finalCategorias):
+            Lista = re.findall(r'[0-9a-zA-Z\-]+',dataLines[x])
+            tmpReviews.asin = tmpProduct.asin
+            tmpReviews.date = Lista[0]
+            tmpReviews.customer_id = Lista[2]
+            tmpReviews.rating = Lista[4]
+            tmpReviews.votes = Lista[6]
+            tmpReviews.helpf = Lista[8]
+            reviewsList.append(Reviews())
+            reviewsList[len(reviewsList)-1].date = tmpReviews.date
+            reviewsList[len(reviewsList)-1].customer_id = tmpReviews.customer_id
+            reviewsList[len(reviewsList)-1].rating = tmpReviews.rating
+            reviewsList[len(reviewsList)-1].votes = tmpReviews.votes
+            reviewsList[len(reviewsList)-1].helpf = tmpReviews.helpf
+            x+=1
 
     else:
         tmpProduct.title = Lista[0]
@@ -108,15 +146,18 @@ def parser():
     while(i < len(file_split_by_id)):
         splitByLine(file_split_by_id[i],tmpProduct, customer, reviews,categorias,similares)
         i+=1
-    #for i in range(len(tmpProduct)):
-    #    print(tmpProduct[i].title)
+    for i in range(len(tmpProduct)):
+        print(tmpProduct[i].title)
 
-    #for i in range(len(similares)):
-    #    print(similares[i].sAsin)
+    for i in range(len(similares)):
+        print(similares[i].sAsin)
 
     for i in range(len(reviews)):
-        print(reviews[i].data)
+        print(reviews[i].date)
 
+
+    #for i in range(len(categorias)):
+    #    print("CatPai:\t",categorias[i].catpai,"\tSub:\t",categorias[i].subcat_name)
     #fr = "|Books[283155]|Subjects[1000]|Religion & Spirituality[22]|Christianity[12290]|Clergy[12360]|Preaching[12368]"
     #fd = fr.split('|',-1)
 
