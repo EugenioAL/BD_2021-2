224c88dd4ad30922505db7e456aad6624af8e257
Eugênio <eaof@icomp.ufam.edu.br>
2022-06-16 09:02:59 -0300

    Parser completo

--- tp1/parser.py
+++ tp1/parser.py
@@ -1,3 +1,4 @@
+from itertools import count
 from typing import List
 import re
 
@@ -44,86 +45,85 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
     Lista = []
     
     dataLines = dataText.split('\n', -1)
-    #for i in range(len(dataLines)):
-        #print(  "linha:\t",i)
-        #print(dataLines[i])
     tmpProduct.id = int(dataLines[0])
-    #print("Produto id:\t",tmpProduct.id)
     Lista = dataLines[1].split("ASIN: ",-1)
     tmpProduct.asin = Lista[1]
-    #print("Produto asin:\t",tmpProduct.asin)
     Lista =  dataLines[2].split("title: ",-1)
     if(len(Lista) > 1):
         tmpProduct.title = Lista[1]
-        #print("Produto titulo:\t",tmpProduct.title)
         Lista =  dataLines[3].split("group: ",-1)
         tmpGrupos.nome = Lista[1]
-        #print("Grupo:\t\t",tmpGrupos.nome)
         Lista =  dataLines[4].split("salesrank: ",-1)
         tmpProduct.salesrank = int(Lista[1])
-        #print("Salesrank:\t",tmpProduct.salesrank)
         productList.append(tmpProduct)
         Lista = dataLines[5].split("similar: ")
         Lista = Lista[1].split("  ",-1)
         simQt = int(Lista[0])
         tmpSimilar.asin = tmpProduct.asin
-        i = 1
-        #print(Lista)
-        while(i <= simQt):
-            similarList.append(Similar())
-            similarList[len(similarList)-1].sAsin = Lista[i]
-            i+=1
+        if(simQt > 0):
+            i = 1
+            #print(Lista)
+            while(i <= simQt):
+                similarList.append(Similar())
+                similarList[len(similarList)-1].sAsin = Lista[i]
+                i+=1
         Lista = dataLines[6].split("categories: ",-1)
         catQt = int(Lista[1])
-        i = 1
-        while(i <= catQt):
-            Lista = re.findall(r'[ña-z &,:\'.()\-A-Z]+', dataLines[6+i])
-            Lista.pop(0)
-            x = len(Lista)
-            Lista2 = re.findall(r'[0-9]+', dataLines[6+i])
-            a = 0
-            print(tmpProduct.id)
-            print(Lista)
-            print(Lista2)
-            while(a < x):
-                if(a > 0):
-                    tmpCategorias.catpai = Lista[a-1]
-                    tmpCategorias.catpai_id = int(Lista2[a-1])
-                    tmpCategorias.subcat_name = Lista[a]
-                    print('a\t',a,'len\t',len(Lista2),'lenx\t',x)
-                    tmpCategorias.subcat_id = int(Lista2[a])
-                else:
-                    tmpCategorias.catpai = Lista[a]
-                    tmpCategorias.catpai_id = int(Lista2[a])
-                    tmpCategorias.subcat_name = Lista[a]
-                    tmpCategorias.subcat_id = (Lista2[a])
-                categoriasList.append(Categorias())
-                categoriasList[len(categoriasList)-1].catpai = tmpCategorias.catpai
-                categoriasList[len(categoriasList)-1].catpai_id =tmpCategorias.catpai_id
-                categoriasList[len(categoriasList)-1].subcat_name = tmpCategorias.subcat_name
-                categoriasList[len(categoriasList)-1].subcat_id = tmpCategorias.subcat_id
-                a+=1
-            i+=1
+        print('id\t',tmpProduct.id)
+        if(catQt > 0):
+            i = 1
+            while(i <= catQt):
+                Lista = dataLines[6+i].split('|',-1)
+                #Lista = re.findall('[^0-9\[\]\|]+', dataLines[6+i])
+                for f in range(len(Lista)):
+                    Lista[f] = re.sub('[[0-9]+]','',Lista[f])
+                Lista.pop(0)
+                x = len(Lista)
+                Lista2 = re.findall(r'[0-9]+', dataLines[6+i])
+                a = 0
+                #print(tmpProduct.id)
+                #if(len(Lista) == len(Lista2)):
+                while(a < x):
+                    if(a > 0):
+                        tmpCategorias.catpai = Lista[a-1]
+                        tmpCategorias.catpai_id = int(Lista2[a-1])
+                        tmpCategorias.subcat_name = Lista[a]
+                        #print('a\t',a,'len\t',len(Lista2),'lenx\t',x)
+                        tmpCategorias.subcat_id = int(Lista2[a])
+                    else:
+                        tmpCategorias.catpai = Lista[a]
+                        tmpCategorias.catpai_id = int(Lista2[a])
+                        tmpCategorias.subcat_name = Lista[a]
+                        tmpCategorias.subcat_id = (Lista2[a])
+                    categoriasList.append(Categorias())
+                    categoriasList[len(categoriasList)-1].catpai = tmpCategorias.catpai
+                    categoriasList[len(categoriasList)-1].catpai_id =tmpCategorias.catpai_id
+                    categoriasList[len(categoriasList)-1].subcat_name = tmpCategorias.subcat_name
+                    categoriasList[len(categoriasList)-1].subcat_id = tmpCategorias.subcat_id
+                    a+=1
+                i+=1
         x = 6 + catQt+1
         Lista = re.findall(r"[0-9]+",dataLines[x])
         reviewsQt = int(Lista[0])
-        finalCategorias = reviewsQt + x
-        x += 1
-        while(x <= finalCategorias):
-            Lista = re.findall(r'[0-9a-zA-Z\-]+',dataLines[x])
-            tmpReviews.asin = tmpProduct.asin
-            tmpReviews.date = Lista[0]
-            tmpReviews.customer_id = Lista[2]
-            tmpReviews.rating = Lista[4]
-            tmpReviews.votes = Lista[6]
-            tmpReviews.helpf = Lista[8]
-            reviewsList.append(Reviews())
-            reviewsList[len(reviewsList)-1].date = tmpReviews.date
-            reviewsList[len(reviewsList)-1].customer_id = tmpReviews.customer_id
-            reviewsList[len(reviewsList)-1].rating = tmpReviews.rating
-            reviewsList[len(reviewsList)-1].votes = tmpReviews.votes
-            reviewsList[len(reviewsList)-1].helpf = tmpReviews.helpf
-            x+=1
+        if(reviewsQt > 0):
+            finalCategorias = reviewsQt + x
+            x += 1
+            while(x <= finalCategorias and x < len(dataLines)):
+                Lista = re.findall(r'[0-9a-zA-Z\-]+',dataLines[x])
+                if(len(Lista) == 9):
+                    tmpReviews.asin = tmpProduct.asin
+                    tmpReviews.date = Lista[0]
+                    tmpReviews.customer_id = Lista[2]
+                    tmpReviews.rating = Lista[4]
+                    tmpReviews.votes = Lista[6]
+                    tmpReviews.helpf = Lista[8]
+                    reviewsList.append(Reviews())
+                    reviewsList[len(reviewsList)-1].date = tmpReviews.date
+                    reviewsList[len(reviewsList)-1].customer_id = tmpReviews.customer_id
+                    reviewsList[len(reviewsList)-1].rating = tmpReviews.rating
+                    reviewsList[len(reviewsList)-1].votes = tmpReviews.votes
+                    reviewsList[len(reviewsList)-1].helpf = tmpReviews.helpf
+                x+=1
 
     else:
         tmpProduct.title = Lista[0]
@@ -150,14 +150,6 @@ def parser():
     while(i < len(file_split_by_id)):
         splitByLine(file_split_by_id[i],tmpProduct, customer, reviews,categorias,similares)
         i+=1
-    for i in range(len(tmpProduct)):
-        print(tmpProduct[i].title)
-
-    for i in range(len(similares)):
-        print(similares[i].sAsin)
-
-    for i in range(len(reviews)):
-        print(reviews[i].date)
 
 
     #for i in range(len(categorias)):
