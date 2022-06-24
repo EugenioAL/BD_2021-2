1be179084e8b476e60a2c55e0c0f5a44a2887a22
Eugênio <eaof@icomp.ufam.edu.br>
2022-06-24 07:32:33 -0300

    revisao feita

--- tp1/parser.py
+++ tp1/parser.py
@@ -47,42 +47,33 @@ class CatsProducts():
         self.asin = val_asin
 
 def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,similarList,catsProdsList):
-    tmpProduct = Product(None,None,None,None,None)
-    tmpGrupos = Grupo(None)
-    tmpReviews = Reviews(None,None,None,None,None,None)
-    tmpCategorias = Categorias(None,None,None,None)
-    tmpSimilar = Similar(None,None)
-    #tmpCatProd = CatsProducts()
     Lista = []
     
     dataLines = dataText.split('\n', -1)
-    tmpProduct.id = int(dataLines[0])
+    id = int(dataLines[0])
     Lista = dataLines[1].split("ASIN: ",-1)
-    tmpProduct.asin = Lista[1]
+    asin = Lista[1]
     Lista =  dataLines[2].split("title: ",-1)
     if(len(Lista) > 1):
-        tmpProduct.title = Lista[1]
+        title = Lista[1]
         Lista =  dataLines[3].split("group: ",-1)
-        tmpGrupos.nome = Lista[1]
+        grupo = Lista[1]
         Lista =  dataLines[4].split("salesrank: ",-1)
-        tmpProduct.salesrank = int(Lista[1])
-        productList.append(tmpProduct)
+        salesrank = int(Lista[1])
+        productList.append(Product(id,asin,title,grupo,salesrank))
         Lista = dataLines[5].split("similar: ")
         Lista = Lista[1].split("  ",-1)
         simQt = int(Lista[0])
-        #tmpSimilar.asin = tmpProduct.asin
         if(simQt > 0):
             i = 1
             while(i <= simQt):
-                similarList.append(Similar(tmpProduct.asin,Lista[i]))
-                #similarList[len(similarList)-1].sAsin = Lista[i]
+                similarList.append(Similar(asin,Lista[i]))
                 i+=1
         Lista = dataLines[6].split("categories: ",-1)
         catQt = int(Lista[1])
         if(catQt > 0):
             i = 1
             while(i <= catQt):
-                #tmpCatProd.asin = tmpProduct.asin
                 Lista = dataLines[6+i].split('|',-1)
                 for f in range(len(Lista)):
                     Lista[f] = re.sub('[[0-9]+]','',Lista[f])
@@ -93,22 +84,11 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
                     Lista2[f] = int(re.sub('\[\]','',Lista2[f]))
                 a = 0
                 while(a < x):
-                    catsProdsList.append(CatsProducts(Lista2[a],tmpProduct.asin))
+                    catsProdsList.append(CatsProducts(Lista2[a],asin))
                     if(a > 0):
-                        tmpCategorias.catpai = Lista[a-1]
-                        tmpCategorias.catpai_id = int(Lista2[a-1])
-                        tmpCategorias.subcat_name = Lista[a]
-                        tmpCategorias.subcat_id = int(Lista2[a])
+                        categoriasList.append(Categorias(Lista[a-1],int(Lista2[a-1]),Lista[a],int(Lista2[a])))
                     else:
-                        tmpCategorias.catpai = Lista[a]
-                        tmpCategorias.catpai_id = int(Lista2[a])
-                        tmpCategorias.subcat_name = Lista[a]
-                        tmpCategorias.subcat_id = (Lista2[a])
-                    categoriasList.append(Categorias(tmpCategorias.catpai_name,tmpCategorias.catpai_id,tmpCategorias.subcat_name,tmpCategorias.subcat_id))
-                    categoriasList[len(categoriasList)-1].catpai = tmpCategorias.catpai
-                    categoriasList[len(categoriasList)-1].catpai_id =tmpCategorias.catpai_id
-                    categoriasList[len(categoriasList)-1].subcat_name = tmpCategorias.subcat_name
-                    categoriasList[len(categoriasList)-1].subcat_id = tmpCategorias.subcat_id
+                        categoriasList.append(Categorias(Lista[a],int(Lista2[a]),Lista[a],int(Lista2[a])))
                     a+=1
                 i+=1
         x = 6 + catQt+1
@@ -120,23 +100,11 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
             while(x <= finalCategorias and x < len(dataLines)):
                 Lista = re.findall(r'[0-9a-zA-Z\-]+',dataLines[x])
                 if(len(Lista) == 9):
-                    tmpReviews.asin = tmpProduct.asin
-                    tmpReviews.date = Lista[0]
-                    tmpReviews.customer_id = Lista[2]
-                    tmpReviews.rating = int( Lista[4])
-                    tmpReviews.votes = int(Lista[6])
-                    tmpReviews.helpf = int(Lista[8])
-                    reviewsList.append(Reviews(None,None,None,None,None,None))
-                    reviewsList[len(reviewsList)-1].date = tmpReviews.date
-                    reviewsList[len(reviewsList)-1].customer_id = tmpReviews.customer_id
-                    reviewsList[len(reviewsList)-1].rating = tmpReviews.rating
-                    reviewsList[len(reviewsList)-1].votes = tmpReviews.votes
-                    reviewsList[len(reviewsList)-1].helpf = tmpReviews.helpf
+                    reviewsList.append(Reviews(asin,Lista[0],Lista[2],int(Lista[4]),int(Lista[6]),int(Lista[8])))
                 x+=1
 
     else:
-        tmpProduct.title = Lista[0]
-        productList.append(tmpProduct)
+        productList.append(Product(id,asin,Lista[0],None,None))
 
 
 product = []
@@ -151,7 +119,7 @@ catsProds = []
 
 def parser(prodList,customerList,rebiesList,categoriasList,similaresList,catsProdsList):
 
-    with open('mid.txt', encoding='latin-1') as file:
+    with open('ptest.txt', encoding='latin-1') as file:
         file_contents = file.read()
         file_split_by_id = file_contents.split('Id:   ',-1)
     
@@ -162,8 +130,13 @@ def parser(prodList,customerList,rebiesList,categoriasList,similaresList,catsPro
         i+=1
 
     for i in range(len(catsProdsList)):
-        
         print("Cat ID:\t",catsProdsList[i].cat_id,"PROD asin:",catsProdsList[i].asin)
+
+    for i in range(len(prodList)):
+        print("titulo:\t",prodList[i].title,"asin:\t",prodList[i].asin)
+
+    for i in range(len(similaresList)):
+        print("asin:\t",similaresList[i].asin,"Sasin:\t",similaresList[i].sAsin)
 
 
 
