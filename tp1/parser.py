d6e41b225430750da4e7dc217efab0fbbc668610
Eugênio <eaof@icomp.ufam.edu.br>
2022-06-23 21:07:40 -0300

    revisando codigo

--- tp1/parser.py
+++ tp1/parser.py
@@ -1,45 +1,58 @@
 import re
+import psycopg2
 
 class Product:
-    def __init__(self):
-        self.id = None
-        self.asin = None
-        self.title = None
-        self.group = None
-        self.salesrank = None
-        self.categoria = None
+    def __init__(self,id,asin,title,group,salesrank):
+        self.id = id
+        self.asin = asin
+        self.title = title
+        self.group = group
+        self.salesrank = salesrank
 
 class Similar:
-    def __init__(self):
-        self.asin = None
-        self.sAsin = None
+    def __init__(self,asin,sAsin):
+        self.asin = asin
+        self.sAsin = sAsin
 
 class Grupo:
-    def __init__(self) -> None:
-        self.nome = None
+    def __init__(self,nome):
+        self.nome = nome
 
 
 class Categorias:
-    def __init__(self):
-        self.catpai = None
-        self.catpai_id = None
-        self.subcat_name = None
-        self.subcat_id = None
-class Reviews:
-    def __init__(self):
-        self.asin = None
-        self.date = None
-        self.customer_id = None
-        self.rating = None
-        self.votes = None
-        self.helpf = None
+    def __init__(self,catpai_name,catpai_id,subcat_name,subcat_id):
+        self.catpai_name = catpai_name
+        self.catpai_id = catpai_id
+        self.subcat_name = subcat_name
+        self.subcat_id = subcat_id
 
-def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,similarList):
-    tmpProduct = Product()
-    tmpGrupos = Grupo()
-    tmpReviews = Reviews()
-    tmpCategorias = Categorias()
-    tmpSimilar = Similar()
+class Sub_prod:
+    def __init__(self,cat,asin):
+        self.cat = cat
+        self.asin = asin
+
+
+class Reviews:
+    def __init__(self,asin,date,customer_id,rating,votes,helpful):
+        self.asin = asin
+        self.date = date
+        self.customer_id = customer_id
+        self.rating = rating
+        self.votes = votes
+        self.helpf = helpful
+
+class CatsProducts():
+    def __init__(self,val_id,val_asin):
+        self.cat_id = val_id
+        self.asin = val_asin
+
+def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,similarList,catsProdsList):
+    tmpProduct = Product(None,None,None,None,None)
+    tmpGrupos = Grupo(None)
+    tmpReviews = Reviews(None,None,None,None,None,None)
+    tmpCategorias = Categorias(None,None,None,None)
+    tmpSimilar = Similar(None,None)
+    #tmpCatProd = CatsProducts()
     Lista = []
     
     dataLines = dataText.split('\n', -1)
@@ -57,18 +70,19 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
         Lista = dataLines[5].split("similar: ")
         Lista = Lista[1].split("  ",-1)
         simQt = int(Lista[0])
-        tmpSimilar.asin = tmpProduct.asin
+        #tmpSimilar.asin = tmpProduct.asin
         if(simQt > 0):
             i = 1
             while(i <= simQt):
-                similarList.append(Similar())
-                similarList[len(similarList)-1].sAsin = Lista[i]
+                similarList.append(Similar(tmpProduct.asin,Lista[i]))
+                #similarList[len(similarList)-1].sAsin = Lista[i]
                 i+=1
         Lista = dataLines[6].split("categories: ",-1)
         catQt = int(Lista[1])
         if(catQt > 0):
             i = 1
             while(i <= catQt):
+                #tmpCatProd.asin = tmpProduct.asin
                 Lista = dataLines[6+i].split('|',-1)
                 for f in range(len(Lista)):
                     Lista[f] = re.sub('[[0-9]+]','',Lista[f])
@@ -79,6 +93,7 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
                     Lista2[f] = int(re.sub('\[\]','',Lista2[f]))
                 a = 0
                 while(a < x):
+                    catsProdsList.append(CatsProducts(Lista2[a],tmpProduct.asin))
                     if(a > 0):
                         tmpCategorias.catpai = Lista[a-1]
                         tmpCategorias.catpai_id = int(Lista2[a-1])
@@ -89,7 +104,7 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
                         tmpCategorias.catpai_id = int(Lista2[a])
                         tmpCategorias.subcat_name = Lista[a]
                         tmpCategorias.subcat_id = (Lista2[a])
-                    categoriasList.append(Categorias())
+                    categoriasList.append(Categorias(tmpCategorias.catpai_name,tmpCategorias.catpai_id,tmpCategorias.subcat_name,tmpCategorias.subcat_id))
                     categoriasList[len(categoriasList)-1].catpai = tmpCategorias.catpai
                     categoriasList[len(categoriasList)-1].catpai_id =tmpCategorias.catpai_id
                     categoriasList[len(categoriasList)-1].subcat_name = tmpCategorias.subcat_name
@@ -111,7 +126,7 @@ def splitByLine(dataText,productList,grupoList,reviewsList,categoriasList,simila
                     tmpReviews.rating = int( Lista[4])
                     tmpReviews.votes = int(Lista[6])
                     tmpReviews.helpf = int(Lista[8])
-                    reviewsList.append(Reviews())
+                    reviewsList.append(Reviews(None,None,None,None,None,None))
                     reviewsList[len(reviewsList)-1].date = tmpReviews.date
                     reviewsList[len(reviewsList)-1].customer_id = tmpReviews.customer_id
                     reviewsList[len(reviewsList)-1].rating = tmpReviews.rating
@@ -129,24 +144,28 @@ customer = []
 reviews = []
 categorias = []
 similares = []
+catsProds = []
 
 
-def parser(prodList,customerList,rebiesList,categoriasList,similaresList):
 
-    with open('amazon-meta.txt', encoding='latin-1') as file:
+
+def parser(prodList,customerList,rebiesList,categoriasList,similaresList,catsProdsList):
+
+    with open('mid.txt', encoding='latin-1') as file:
         file_contents = file.read()
         file_split_by_id = file_contents.split('Id:   ',-1)
     
     i=1
     
     while(i < len(file_split_by_id)):
-        splitByLine(file_split_by_id[i],prodList,customerList,rebiesList,categoriasList,similaresList)
+        splitByLine(file_split_by_id[i],prodList,customerList,rebiesList,categoriasList,similaresList,catsProdsList)
         i+=1
 
-    for i in range(len(prodList)):
-        print(prodList[i].title)
+    for i in range(len(catsProdsList)):
+        
+        print("Cat ID:\t",catsProdsList[i].cat_id,"PROD asin:",catsProdsList[i].asin)
 
 
 
 
-parser(product,customer,reviews,categorias,similares)
+parser(product,customer,reviews,categorias,similares,catsProds)
